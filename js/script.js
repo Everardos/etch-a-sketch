70d159a3988c48e93440bf4fe11c397ce2bbb8ad
Joshua Adkisson <112263154+Everardos@users.noreply.github.com>
2024-02-07 23:37:29 -0500

    Implement style and mouseover listener

--- js/script.js
+++ js/script.js
@@ -5,12 +5,13 @@ function createRows(number) {
         const row = document.createElement("div");
         game.appendChild(row);
         row.classList.add("row");
-        console.log("Row: " + i);
         for (let i=0; i < number; i++) {
             const box = document.createElement("div");
             row.appendChild(box);
             box.classList.add("box");
-            console.log("Box: " + i)
+            box.addEventListener("mouseover", () => {
+                box.classList.add("hover");
+            })
         }
     }
 }
